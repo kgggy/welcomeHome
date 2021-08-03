0ec416aace22b761cf703132c2c71c49210c3194
DESKTOP-4RUDTH8\admin <douwn77@naver.com>
2021-08-03 16:56:07 +0900

    8/3

--- welcomeHome/src/main/java/co/yedam/board/CommentDAO.java
+++ welcomeHome/src/main/java/co/yedam/board/CommentDAO.java
@@ -5,24 +5,108 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
-public class CommentDAO extends DAO {
-	private static CommentDAO instance; //commentDAO class를 담아놓기 위한 static 타입의 instance 필드 선언.
+public class CommentDAO extends DAO { 
+	private static CommentDAO instance; // commentDAO class를 담아놓기 위한 static 타입의 instance 필드 선언.
+
 	private CommentDAO() {
-		
+
 	}
-	public static CommentDAO getInstance() { //싱글톤방식
+
+	public static CommentDAO getInstance() { // 싱글톤방식
 		instance = new CommentDAO();
 		return instance;
 	}
-	
-	//글목록 가져오기
-	public List<HashMap<String, Object>> selectAll() { //HashMap => key, value 형식으로 가져옴.
+
+	// 글 내용 수정.
+	public HashMap<String, Object> update(Comment comment) {
 		connect();
-		List<HashMap<String, Object>> list = new ArrayList<HashMap<String,Object>>();
+		String sql = "update comments set name=?, content=? where id=?";
+		try {
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1, comment.getName());
+			pstmt.setString(2, comment.getContent());
+			pstmt.setString(3, comment.getId());
+			int r = pstmt.executeUpdate();
+			System.out.println("수정: " + r);
+
+			HashMap<String, Object> map = new HashMap<String, Object>();
+			map.put("id", comment.getId());
+			map.put("name", comment.getName());
+			map.put("content", comment.getContent());
+			return map;
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+			
+		} finally {
+			disconnect();
+		}
+		return null;
+	}
+
+	// 글 등록하기.
+	public HashMap<String, Object> insert(Comment comment) {
+		// 1. id_repository 테이블에서 현재 시퀀스번호.
+		// 2. comments 테이블에 추가.
+		// 3. id_repository 에 새로운 시퀀스번호로 변경.
+		int nextId = 0; // 시퀀스값 받아오는 변수.
+		connect();
+		try {
+			// 1.현재 시퀀스번호 가져옴.
+			conn.setAutoCommit(false); // 도중에 에러나면 기존값으로.
+			stmt = conn.createStatement(); // stmt는 쿼리선언하면서 변수를 직접 값을넣어줘야함(ex. comment.getId()), pstmt => ?를 써서 쿼리만들때 바로
+											// 안넣어도됨.
+			rs = stmt.executeQuery("select value from id_repository where name='comment'"); // 현재 value 가져오는 쿼리.
+			if (rs.next()) { // 한건만 가져옴.
+				nextId = rs.getInt("value");
+			}
+			// 2. 시퀀스에 1 증가시켜서 comment 테이블에 입력.
+			nextId++; // 다음순번에 추가.
+			pstmt = conn.prepareStatement("insert into comments values(?,?,?)");
+			pstmt.setInt(1, nextId);
+			pstmt.setString(2, comment.getName());
+			pstmt.setString(3, comment.getContent());
+			int r = pstmt.executeUpdate(); // 조회:executeQuery, 입력/수정/삭제:executeUpdate => 수정된게 하나면 1을 리턴.
+			System.out.println("입력건수: " + r);
+			// 3.새로운 시퀀스번호로 변경.
+			pstmt = conn.prepareStatement("update id_repository set value=? where name='comment'");
+			pstmt.setInt(1, nextId);
+			pstmt.executeUpdate();
+
+			conn.commit();
+			// 정상적으로 실행시.
+			HashMap<String, Object> map = new HashMap<String, Object>();
+			map.put("id", nextId);
+			map.put("name", comment.getName());
+			map.put("content", comment.getContent());
+
+			return map;
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+			try {
+				conn.rollback(); // 정상실행 실패시.
+				HashMap<String, Object> map = new HashMap<String, Object>();
+				map.put("msg", e.getMessage());
+				return map;
+			} catch (SQLException e1) {
+				e1.printStackTrace();
+			}
+		} finally {
+			disconnect();
+		}
+
+		return null;
+	}
+
+	// 글목록 가져오기.
+	public List<HashMap<String, Object>> selectAll() { // HashMap => key, value 형식으로 가져옴.
+		connect();
+		List<HashMap<String, Object>> list = new ArrayList<HashMap<String, Object>>();
 		try {
 			pstmt = conn.prepareStatement("select * from comments");
 			rs = pstmt.executeQuery();
-			while(rs.next()) {
+			while (rs.next()) {
 //				System.out.println(rs.getString("id") + ", " + rs.getString("name"));
 //				System.out.println("=================================");
 				HashMap<String, Object> map = new HashMap<>();
@@ -38,4 +122,5 @@ public class CommentDAO extends DAO {
 		}
 		return list;
 	}
+
 }
--- welcomeHome/src/main/java/co/yedam/board/CommentSevlet.java
+++ welcomeHome/src/main/java/co/yedam/board/CommentSevlet.java
@@ -14,49 +14,97 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/CommentSevlet")
 public class CommentSevlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    public CommentSevlet() {
-        super();
-    }
 
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		request.setCharacterEncoding("utf-8");
-		response.setContentType("text/xml;charset=utf-8"); //xml data로 만듦.(key, value 형식)
-		response.setCharacterEncoding("utf-8");
-		
-		PrintWriter out = response.getWriter();
-		String cmd = request.getParameter("cmd");
-		
-		if(cmd == null) { 
-			StringBuffer sb = new StringBuffer();
-			sb.append("<result>");
-			sb.append("<code>error</code>"); //cmd값이 없는 경우 error 노출.
-			sb.append("<data>");
-			sb.append("cmd null");
-			sb.append("</data>");
-			sb.append("</result>");
-			out.print(sb.toString());
-					
-		}else if(cmd.equals("selectAll")) {
-			List<HashMap<String, Object>> list = CommentDAO.getInstance().selectAll();
-			StringBuffer sb = new StringBuffer();
-			sb.append("<result>");
-			sb.append("<code>success</code>");
-			for(HashMap<String, Object> map : list) {
-				sb.append("<data>");
-				sb.append("   <id>" + map.get("id") + "</id>");
-				sb.append("   <name>" + map.get("name") + "</name>");
-				sb.append("   <content>" + map.get("content") + "</content>");
-				sb.append("</data>");
-			}
-			sb.append("</result>");
-			out.print(sb.toString());
-		}else if(cmd.equals("insert")) {
-			
-		}
+	public CommentSevlet() {
+		super();
 	}
 
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		request.setCharacterEncoding("utf-8");
+		response.setContentType("text/xml;charset=utf-8"); // xml data로 만듦.(key, value 형식)
+		response.setCharacterEncoding("utf-8");
+
+		PrintWriter out = response.getWriter();
+		String cmd = request.getParameter("cmd");
+
+		if (cmd == null) {
+			out.println(errorXML("cmd null")); //cmd값이 없으면 에러.
+
+		} else if (cmd.equals("selectAll")) { //1)전체조회
+			try {
+				List<HashMap<String, Object>> list = CommentDAO.getInstance().selectAll();
+				StringBuffer sb = new StringBuffer();
+				sb.append("<result>");
+				sb.append("<code>success</code>");
+				sb.append("<data>");
+				for (HashMap<String, Object> map : list) {
+					sb.append("<row>"); // 한건씩 구분해서 가져오기위해.
+					sb.append("   <id>" + map.get("id") + "</id>");
+					sb.append("   <name>" + map.get("name") + "</name>");
+					sb.append("   <content>" + map.get("content") + "</content>");
+					sb.append("</row>");
+				}
+				sb.append("</data>");
+				sb.append("</result>");
+				out.print(sb.toString());
+			} catch (Exception e) {
+				out.println(errorXML(e.getMessage()));
+			}
+		} else if (cmd.equals("insert")) { // 2)한건입력.(사용자가 입력한 parameter값 읽어와서 comment에 담아줌.
+			try {
+				String name = request.getParameter("name");
+				String content = request.getParameter("content");
+				Comment comment = new Comment();
+				comment.setName(name);
+				comment.setContent(content);
+				HashMap<String, Object> map = CommentDAO.getInstance().insert(comment); // insert리턴타입 => HashMap.
+				
+				out.println(dataXML(map));
+				
+			} catch (Exception e) {
+				out.println(errorXML(e.getMessage()));
+			}
+		} else if(cmd.equals("update")) { //3)수정.
+			String id = request.getParameter("id");
+			String name = request.getParameter("name");
+			String content = request.getParameter("content");
+			
+			Comment comment = new Comment();
+			comment.setId(id);
+			comment.setName(name);
+			comment.setContent(content);
+			
+			HashMap<String, Object> map = CommentDAO.getInstance().update(comment);
+			out.println(dataXML(map));
+		}
+	} // end of doGet();
+	
+	private String dataXML(HashMap<String, Object> map) { //결과값을 다시 받아옴.
+		StringBuffer sb = new StringBuffer();
+		sb.append("<result>");
+		sb.append("<code>success</code>");
+		sb.append("<data>");
+		sb.append("   <id>" + map.get("id") + "</id>");
+		sb.append("   <name>" + map.get("name") + "</name>");
+		sb.append("   <content>" + map.get("content") + "</content>");
+		sb.append("</data>");
+		sb.append("</result>");
+		return sb.toString();
+	}
+	
+	private String errorXML(String msg) { //에러발생시.
+		StringBuffer sb = new StringBuffer();
+		sb.append("<result>");
+		sb.append("<code>error</code>");
+		sb.append("<data>" + msg + "</data>");
+		sb.append("</result>");
+		
+		return sb.toString();
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		doGet(request, response);
 	}
 
